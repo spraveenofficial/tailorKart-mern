0eeb4b15b2b43c3578b59d6c6ecf687f26b7c782
Praveen <spraveen593@gmail.com>
2022-03-17 01:30:51 +0530

    Feat: Added Login and Signup Feature

--- frontend/src/Pages/Signup/index.jsx
+++ frontend/src/Pages/Signup/index.jsx
@@ -1,9 +1,13 @@
 import "./style.css";
 import Input from "../../Components/Input";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
+import Toast from "../../Components/Toast";
+import { useSignup } from "../../Hooks/auth";
+import { signupUser } from "../../Actions";
+import Spinner from "../../Components/Spinner";
 const Signup = () => {
+  const { loading, success, message, dispatch } = useSignup();
   const [userData, setUserData] = useState({
     name: "",
     email: "",
@@ -12,7 +16,22 @@ const Signup = () => {
   const handleChange = (e) => {
     setUserData({ ...userData, [e.target.name]: e.target.value });
   };
-
+  const handleSubmit = () => {
+    signupUser(
+      {
+        name: userData.name,
+        email: userData.email,
+        password: userData.password,
+      },
+      dispatch
+    );
+  };
+  useEffect(() => {
+    success === true &&
+      setTimeout(() => {
+        window.location.href = "/";
+      }, 1000);
+  }, [success]);
   return (
     <div className="login-page">
       <div className="login">
@@ -54,13 +73,16 @@ const Signup = () => {
               <br />
             </div>
           </div>
-          <button className="btn loading-btn">Signup</button>
+          <button onClick={() => handleSubmit()} className="btn loading-btn">
+            {loading && <Spinner />}Signup
+          </button>
           <div className="have-account flex">
             <Link to="/login">
               <p>Alreay have Account?</p>
             </Link>
           </div>
         </div>
+        {!loading && message && <Toast message={message} success={success} />}
       </div>
     </div>
   );
