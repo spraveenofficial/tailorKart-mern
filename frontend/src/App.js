dec732cbe825e8417fa90e6bf080ad42e95fe106
Praveen <spraveen593@gmail.com>
2022-03-16 20:40:29 +0530

    Init: Dev Branch With react app with default css

---
+++ frontend/src/App.js
@@ -0,0 +1,6 @@
+import "./index.css";
+function App() {
+  return <h1>This is Main Page</h1>;
+}
+
+export default App;
---
+++ frontend/src/Components/MiniNavbar/index.jsx
@@ -0,0 +1,5 @@
+import "./style.css";
+const MiniNav = () => {
+  return <h1>This is Mini Navbar</h1>;
+};
+export default MiniNav;
