6fe1d755f365bb8959bd6feddf7a393dbc74c8ed
Praveen <spraveen593@gmail.com>
2022-03-17 02:50:28 +0530

    Fixed: Baseurl depending to env variables

--- frontend/src/Utils/baseurl.js
+++ frontend/src/Utils/baseurl.js
@@ -1,4 +1,4 @@
 const baseUrl =
-  process.env.REACT_APP_MODE !== "dev" ? "http://localhost:4000/v1/api" : null;
+  process.env.REACT_APP_MODE === "dev" ? "http://localhost:4000/v1/api" : "https://tailor-kart.vercel.app/v1/api";
 
 export default baseUrl;
