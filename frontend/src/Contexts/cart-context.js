79cb0caeb7c54449dec5ba47cbe50accf34ed649
Praveen <spraveen593@gmail.com>
2022-03-17 18:48:29 +0530

    Feat: Added Cart Context

---
+++ frontend/src/Contexts/cart-context.js
@@ -0,0 +1,36 @@
+import { createContext, useContext, useReducer } from "react";
+import { cartReducer } from "../Reducers/cart";
+
+const CartContext = createContext();
+
+const useCart = () => useContext(CartContext);
+const CartProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(cartReducer, {
+    cart: localStorage.getItem("cart")
+      ? JSON.parse(localStorage.getItem("cart"))
+      : [],
+    wishList: localStorage.getItem("wishlist")
+      ? JSON.parse(localStorage.getItem("wishlist"))
+      : [],
+    totalItems: localStorage.getItem("cart")
+      ? JSON.parse(localStorage.getItem("cart")).reduce(
+          (a, b) => Number(a) + Number(b.quantity),
+          0
+        )
+      : 0,
+  });
+  return (
+    <CartContext.Provider
+      value={{
+        cart: state.cart,
+        setCart: dispatch,
+        totalItems: state.totalItems,
+        price: state.price,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export { useCart, CartProvider };
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { AuthProvider } from "./Contexts/auth-context";
+import { CartProvider } from "./Contexts/cart-context";
 ReactDOM.render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <CartProvider>
+        <App />
+      </CartProvider>
     </AuthProvider>
   </React.StrictMode>,
   document.getElementById("root")
